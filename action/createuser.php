91ee9566b8010a99c5a11fb709f561970d9e68a7
Mikko Lehtosaari <Lehtiz@gmail.com>
2011-09-22 09:29:18 +0300

    added action scripts

---
+++ action/createuser.php
@@ -0,0 +1,25 @@
+<?php include_once "top.php"; ?>
+
+<?php 
+include_once "dbconnect.php";
+
+$username = strip_tags($_POST["txtUsername"]);
+$email = strip_tags($_POST["txtEmail"]);
+$password = strip_tags($_POST["txtPassword"]);
+
+
+$query = "INSERT INTO user(name, email, password) VALUES('$username', '$email', '$password')";
+
+$result = mysql_query($query);
+if ($result){
+    header("location: ../index.php");
+}
+else{
+    $error = mysql_error();
+    print "$error <br/>";
+    print "<a href='createuser.php'>Back</a>";
+}
+
+?>
+
+<?php include_once "bottom.php"; ?>
