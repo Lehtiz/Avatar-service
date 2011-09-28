7320bbc73bf41d46082fc6416ddc306157401a21
Mikko Lehtosaari <Lehtiz@gmail.com>
2011-09-28 13:40:26 +0300

    added userid pk

--- avatardb.sql
+++ avatardb.sql
@@ -5,6 +5,7 @@ USE avatar;
 DROP TABLE IF EXISTS user;
 
 CREATE TABLE IF NOT EXISTS user(
+    userid INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     username VARCHAR(30) NOT NULL,
     useremail VARCHAR(50) NOT NULL,
     userpassword VARCHAR(30) NOT NULL
