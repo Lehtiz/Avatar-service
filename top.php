5f533a78cffb3921fabc6bf322a9c377b720488c
Heca Zgine <k9kihe00@students.oamk.fi>
2011-10-04 12:25:05 +0300

    dev

--- top.php
+++ top.php
@@ -5,10 +5,10 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
     <head>
-    </head>
         <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
         <title>Avatar-service</title>
         <link rel="stylesheet" type="text/css" href="main.css" />
+    </head>
     
     <body onLoad="document.getElementById('1').focus()">
         <div id='center'>
