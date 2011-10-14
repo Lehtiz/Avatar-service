3f9157a1b3bc3506e401804352acb534e9502dbe
Mikko Lehtosaari <Lehtiz@gmail.com>
2011-10-14 13:24:01 +0300

    added some ideas for future

--- avatardb.sql
+++ avatardb.sql
@@ -10,3 +10,17 @@ CREATE TABLE user(
 )ENGINE = InnoDB;
 
 INSERT INTO user(username, useremail, userpassword) VALUES('admin', 'admin@host.com', 'admin')
+
+
+CREATE TABLE(
+    avatarid INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    avatarname VARCHAR(30) NOT NULL,
+    avatarscale DECIMAL NOT NULL,
+    avatarfile VARCHAR(100) NOT NULL
+)ENGINE = InnoDB;
+
+
+CREATE TABLE useravatar(
+    userid,
+    avatarid
+)ENGINE = InnoDB;
