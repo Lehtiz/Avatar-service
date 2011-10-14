4f09002460c0d04c7f34d466a4dbb6d696a11d70
Mikko Lehtosaari <Lehtiz@gmail.com>
2011-10-14 13:30:27 +0300

    removed some tabs

--- js/3d.js
+++ js/3d.js
@@ -15,24 +15,11 @@ var start=parseInt(new Date().getTime());
 var now;
 
 function initGraffa() {
-
     canvas = document.getElementById('graffa');
-
-    canvas.onmouseover = function(event) {
-	mouseovercanvas = true;
-    };
-
-    canvas.onmousemove = function(event) {
-	mouseovercanvas = true;
-    };
-
-    canvas.onmouseout = function(event) {
-	mouseovercanvas = false;
-    };
 
     //disable context menu for canvas
     canvas.oncontextmenu = function(event) {
-	return false;
+        return false;
     }
 
     renderer = new GLGE.Renderer(canvas);
@@ -76,36 +63,35 @@ function render() {
 
     //document.getElementById("info").innerHTML="Camera:" + camera.getLocX() +", " + camera.getLocY() + ", " + camera.getLocZ() + " : " + camera.getRotX() + ", " + camera.getRotY() + ", " + camera.getRotZ();
 
-
     lasttime = now;
 }
 
 function checkkeys() { 
     /* movement disabled, can enable later for sample anims
     if (keys.isKeyPressed(GLGE.KI_PAGE_UP)) {
-	addmove('Move,up');
+        addmove('Move,up');
     }
     if (keys.isKeyPressed(GLGE.KI_PAGE_DOWN)) {
-	addmove('Move,down');
+        addmove('Move,down');
     }
     if (keys.isKeyPressed(GLGE.KI_W) || keys.isKeyPressed(GLGE.KI_UP_ARROW)) {
-	addmove('Move,forward');
+        addmove('Move,forward');
     }
     if (keys.isKeyPressed(GLGE.KI_S) || keys.isKeyPressed(GLGE.KI_DOWN_ARROW)) {
-	addmove('Move,back');
+        addmove('Move,back');
     }
     if (keys.isKeyPressed(GLGE.KI_A)) {
-	addmove('Move,left');
+        addmove('Move,left');
     }
     if (keys.isKeyPressed(GLGE.KI_D)) {
-	addmove('Move,right');
+        addmove('Move,right');
     }*/
     //rotation direction from the cameras view
     if (keys.isKeyPressed(GLGE.KI_LEFT_ARROW)) {
-	addmove('Rotate,right');
+        addmove('Rotate,right');
     }
     if (keys.isKeyPressed(GLGE.KI_RIGHT_ARROW)) {
-	addmove('Rotate,left');
+        addmove('Rotate,left');
     }
 
     checkmove();
@@ -114,21 +100,21 @@ function checkkeys() {
 
 function checkmouse() {
     if (mouseovercanvas) {
-	var mouseposition = mouse.getMousePosition();
-	mouseposition.x -= document.getElementById("left").offsetLeft;
-	mouseposition.y -= document.getElementById("left").offsetTop;
+    var mouseposition = mouse.getMousePosition();
+    mouseposition.x -= document.getElementById("left").offsetLeft;
+    mouseposition.y -= document.getElementById("left").offsetTop;
 	
-	if (mouse.isButtonDown(GLGE.MI_RIGHT) & mouseposition.x && mouseposition.y) {
-
-	    var dx = old_mousex - mouseposition.x;
-	    if (dx < 0) {
-		addmove('Rotate,right');
-	    } else if (dx > 0) {
-		addmove('Rotate,left');
-	    }
+    if (mouse.isButtonDown(GLGE.MI_RIGHT) & mouseposition.x && mouseposition.y) {
+        var dx = old_mousex - mouseposition.x;
+        if (dx < 0){
+        	addmove('Rotate,right');
+        } 
+        else if (dx > 0){
+        	addmove('Rotate,left');
+        }
 	
-	    old_mousex = mouseposition.x;
-	    old_mousey = mouseposition.y;
-	}
+        old_mousex = mouseposition.x;
+        old_mousey = mouseposition.y;
+        }
     }
 }
