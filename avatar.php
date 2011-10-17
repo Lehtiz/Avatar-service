2d2d6305bf721815378eecf8b0091157583e6f58
Mikko Lehtosaari <Lehtiz@gmail.com>
2011-10-17 10:44:17 +0300

    added dropbox for avatar.php, does not change avatar appearance yet

--- avatar.php
+++ avatar.php
@@ -2,29 +2,48 @@
 
 <?php
     if ($_SESSION["logged_in"]==true){
-    include_once "js.php";
-        print "
-        <h3>Edit Identity & Avatar</h3>
-
-        <div id='left'>
-            <canvas id='graffa' width='250' height='500'>
-                This text is displayed if your browser does not support HTML5 Canvas.
-            </canvas>
-        </div>
-        <div id='right'>
-            <br />
-            <form method='post' action='action/saveavatarchanges.php'>
-                <input type='radio' name='group1' value='option1' checked> Option1<br />
-                <input type='radio' name='group1' value='option2'> Option2<br />
-                <input type='radio' name='group1' value='option3'> Option3<br />
-                <br />
-                <input type='submit' value='Save changes' />
-            </form>
+        include_once "js.php";
+        include_once "action/dbconnect.php";
         
-        <form method='link' action='action/logoutscript.php'>
-            <input type='submit' value='Log out'>
-        </form>
-        </div>
+        //db stuff
+        $query = "SELECT * FROM avatar";
+        $result = mysql_query($query);
+        if(!$result){
+            print mysql_error();
+            mysql_close($dbConnection);
+            exit;
+        }
+        //
+    
+        print "
+            <h3>Edit Identity & Avatar</h3>
+
+            <div id='left'>
+                <canvas id='graffa' width='250' height='500'>
+                    This text is displayed if your browser does not support HTML5 Canvas.
+                </canvas>
+            </div>
+            <div id='right'>
+                <br />
+                <form method='post' action='action/saveavatarchanges.php'>
+                    Select avatar appearance:<br /><select name='drbavatar'>
+        ";
+        while ($dataArray = mysql_fetch_assoc($result)){
+            print "<option value='$dataArray[avatarid]'>$dataArray[avatarname]</option>";
+        }
+        print "
+                    </select><br />
+                    <input type='radio' name='group1' value='option1' checked> Option1<br />
+                    <input type='radio' name='group1' value='option2'> Option2<br />
+                    <input type='radio' name='group1' value='option3'> Option3<br />
+                    <br />
+                    <input type='submit' value='Save changes' />
+                </form>
+            
+            <form method='link' action='action/logoutscript.php'>
+                <input type='submit' value='Log out'>
+            </form>
+            </div>
         ";
     }
     else
--- avatardb.sql
+++ avatardb.sql
@@ -9,17 +9,21 @@ CREATE TABLE user(
     userpassword VARCHAR(30) NOT NULL
 )ENGINE = InnoDB;
 
-INSERT INTO user(username, useremail, userpassword) VALUES('admin', 'admin@host.com', 'admin')
+INSERT INTO user(username, useremail, userpassword) VALUES('admin', 'admin@host.com', 'admin');
 
-/*
-CREATE TABLE(
+
+CREATE TABLE avatar(
     avatarid INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     avatarname VARCHAR(30) NOT NULL,
     avatarscale DECIMAL NOT NULL,
     avatarfile VARCHAR(100) NOT NULL
 )ENGINE = InnoDB;
 
+INSERT INTO avatar(avatarname, avatarscale, avatarfile) VALUES('default', '0.1', 'avatar1.dae');
+INSERT INTO avatar(avatarname, avatarscale, avatarfile) VALUES('second', '0.1', 'avatar2.dae');
+INSERT INTO avatar(avatarname, avatarscale, avatarfile) VALUES('third', '0.1', 'avatar3.dae');
 
+/*
 CREATE TABLE useravatar(
     userid,
     avatarid
