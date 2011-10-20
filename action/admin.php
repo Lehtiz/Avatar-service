3e0ebf6f9ea70d316b902f746d2e867f2522bfda
Mikko Lehtosaari <Lehtiz@gmail.com>
2011-10-20 10:58:54 +0300

    first commit with admin stuffs. uploading works if folder privs are set correctly, missing lots of restictions. user functions in working order, delete, change name andor pass

---
+++ action/admin.php
@@ -0,0 +1,103 @@
+<?php
+include_once "dbconnect.php";
+$selection = $_POST['mode'];
+
+if($selection == 1){ //add avatar
+    $avatarname = $_POST['avatarname'];
+    $avatarscale = $_POST['avatarscale'];
+
+    $uploadDir = "upload/"; //mod for perm "www-data"
+    $storageDir = "../models/"; //write perm for apache
+
+    //if ($_FILES["file"]["type"] == "image/gif")/* && ($_FILES["file"]["size"] < 20000))*/{
+    $tmpname = $_FILES["avatarfile"]["tmp_name"];
+    $filename = $_FILES["avatarfile"]["name"];
+
+    if ($_FILES["avatarfile"]["error"] > 0){
+        echo "Return Code: " . $_FILES["avatarfile"]["error"] . "<br />";
+    }
+    else{
+        echo "Upload: " . $filename . "<br />";
+        echo "Type: " . $_FILES["avatarfile"]["type"] . "<br />";
+        echo "Size: " . ($_FILES["avatarfile"]["size"] / 1024) . " Kb<br />";
+        echo "Temp file: " . $tmpname . "<br />";
+
+        if (file_exists($storageDir . $filename)){
+            echo $filename . " already exists. ";
+        }
+        else{
+            move_uploaded_file($tmpname, $storageDir . $filename);
+            echo "Stored in: " . $storageDir . $filename;
+        }
+    }
+    //}else{echo "Invalid file";}
+
+
+    $query = "INSERT INTO avatar(avatarName, avatarScale, avatarFile) VALUES('$avatarname', '$avatarscale', '$filename')";
+    $result = mysql_query($query);
+    if(!$result){
+        print mysql_error();
+        mysql_close($dbConnection);
+        exit;
+    }
+    else{
+        print "Avatar successfully added.";
+    }
+}//endif sel=1
+else if($selection == 2){ //removeavatar
+    $action = $_POST['action'];
+    $userid = $_POST['drbuser'];
+    if($userid != 0){
+        if($action == 'remove'){
+            //del useravatar info
+            $query1 = "DELETE FROM useravatar WHERE userId='$userid'";
+            $result1 = mysql_query($query1);
+            if(!$result1){
+                print mysql_error();
+                mysql_close($dbConnection);
+                exit;
+            }
+            //del user
+            $query2 = "DELETE FROM user WHERE userId='$userid'";
+            $result2 = mysql_query($query2);
+            if(!$result2){
+                print mysql_error();
+                mysql_close($dbConnection);
+                exit;
+            }
+            if($result1 && $result2){
+                print "operation succesfull. <a href='../adminform.php'>Back</a>";
+            }
+        }
+        if($action == 'edit'){
+            $newname = mysql_real_escape_string(strip_tags($_POST['newname']));
+            $newpass = crypt(mysql_real_escape_string(strip_tags($_POST['newpassword'])));
+            
+            $cbname = $_POST['chkname'];
+            $cbpass = $_POST['chkpass'];
+            if($cbname && !$cbpass){
+                $query = "UPDATE user SET userName='$newname' WHERE userId='$userid'";
+            }
+            if(!$cbname && $cbpass){
+                $query = "UPDATE user SET userPassword='$newpass' WHERE userId='$userid'";
+            }
+            if($cbname && $cbpass){
+                $query = "UPDATE user SET userName='$newname', userPassword='$newpass' WHERE userId='$userid'";
+            }
+            if(!$cbname && !$cbpass){
+                print "no fields selected <a href='../adminform.php'>Back</a>";
+            }
+            
+            $result = mysql_query($query);
+            if(!$result){
+                print mysql_error();
+                mysql_close($dbConnection);
+                exit;
+            }
+            else{
+                print "operation succesfull. <a href='../adminform.php'>Back</a>";
+            }
+        }
+    }
+}
+?>
---
+++ adminform.php
@@ -0,0 +1,57 @@
+<?php
+session_start();
+if($_SESSION["logged_in"]==false){
+    header("location: index.php");
+}
+include_once "action/dbconnect.php";
+
+
+//fetch usernames
+$query = "SELECT * FROM user";
+$result = mysql_query($query);
+if(!$result){
+    print mysql_error();
+    mysql_close($dbConnection);
+    exit;
+}
+$dataArray = mysql_fetch_assoc($result);
+
+print "
+<html><body>
+<h3>Add avatar</h3>
+    <form action='action/admin.php' enctype='multipart/form-data' method='POST'><table>
+    <input type='hidden' name='mode' value='1' />
+    <tr><td>Name: </td><td><input type='text' name='avatarname' /></td></tr>
+    <tr><td>Scale: </td><td><input type='text' name='avatarscale' /></td></tr>
+    <tr><td>File: </td><td><input type='file' name='avatarfile' id='file' /></td></tr>
+    <tr><td><input type='submit' value='Submit' /></td></tr>
+    </table></form>
+    
+    <form action='action/admin.php' method='POST'><table>
+    <input type='hidden' name='mode' value='2' />
+    <tr><td><input type='radio' name='action' value='remove' checked /> Remove</td>
+    <td><input type='radio' name='action' value='edit' /> Edit</td></tr>
+    <tr><td>Select user: </td><td><select name='drbuser'>
+    <option value='0'>Select user</option>
+";
+while ($dataArray = mysql_fetch_assoc($result)){
+    if($dataArray['avatarId'] == $selected){
+        print "<option value='$dataArray[userId]'>$dataArray[userName]</option>";
+    }
+}
+print "
+    </td></tr><tr><td colspan=3>Input and select info to update</td></tr>
+    <tr><td><input type='checkbox' name='chkname' /> New name: </td><td><input type='text' name='newname' /></td></tr>
+    <tr><td><input type='checkbox' name='chkpass' /> New password: </td><td><input type='text' name='newpassword' /></td></tr>
+    <tr><td><input type='submit' value='Submit' /></td></tr>
+    </table></form>
+</body></html>
+";
+/*
+
+
+
+*/
+
+print "<a href='index.php'>Back</a> <a href='action/logoutscript.php'>Log out</a>";
+?>
