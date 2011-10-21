96e547c19aa0c465bf7a485b53e2c1d608946117
Mikko Lehtosaari <Lehtiz@gmail.com>
2011-10-21 11:53:52 +0300

    tweaks

--- action/loginscript.php
+++ action/loginscript.php
@@ -24,7 +24,6 @@ else{
     $dataArray = mysql_fetch_assoc($result);
     $db_user_name = $dataArray[userName];
     $db_user_password = $dataArray[userPassword];
-    //if (strcmp($username,$db_user_name)==0 && strcmp($password,$db_user_password)==0)
     if (strcmp($username,$db_user_name)==0 && crypt($password,$db_user_password)==$db_user_password)
     {
         $_SESSION["logged_in"]=true;
